19e4a362e9e5b3d3cba68b486cd190a84e37fb77
Valentina Kim <vortegap@unal.edu.co>
2025-06-13 18:18:53 +0700

    Add product file uploads and download links

--- resources/views/seller/my-products.blade.php
+++ resources/views/seller/my-products.blade.php
@@ -1,6 +1,6 @@
 <div>
     <h1 class="text-xl font-semibold mb-4">My Products</h1>
-    <a href="#" class="bg-[#4FC3F7] text-black px-4 py-2 rounded">Add Product</a>
+    <a href="{{ route('seller.products.create') }}" class="bg-[#4FC3F7] text-black px-4 py-2 rounded" wire:navigate>Add Product</a>
     <ul class="mt-4 space-y-2">
         @foreach($products as $product)
             <li class="border border-[#374151] p-2 flex justify-between">
--- resources/views/shop/thank-you.blade.php
+++ resources/views/shop/thank-you.blade.php
@@ -5,7 +5,7 @@
             @foreach($order->items as $item)
                 <li class="flex justify-between">
                     <span>{{ $item->product->name }} x {{ $item->quantity }}</span>
-                    <a href="#" class="text-[#4FC3F7]">Tải file</a>
+                    <a href="{{ \Illuminate\Support\Facades\Storage::url($item->product->file_path) }}" class="text-[#4FC3F7]">Tải file</a>
                 </li>
             @endforeach
         </ul>
